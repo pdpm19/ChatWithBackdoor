c70a4f1218e2aeee41bdc508fc489fe03535dd49
pdpm19 <pdp.moreira@gmail.com>
2021-05-14 13:16:44 +0100

    Changes made to Handshake phase

--- Client/connect_server.py
+++ Client/connect_server.py
@@ -1,13 +1,10 @@
-import binascii, socket, threading, os
+import binascii, socket, threading, os, sys
 from base64 import b64decode, b64encode
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 from Crypto import Random
 from Crypto.Hash import SHA256, HMAC
-from Crypto.Cipher import AES
-
-host_IP = "192.168.207.15"
-host_PORT = 8080
+from Crypto.Cipher import AES, PKCS1_OAEP
 
 # AES implementation
 # Credits: https://medium.com/quick-code/aes-implementation-in-python-a82f582f51c2
@@ -45,6 +42,25 @@ class AESCipher(object):
         last_character = plain_text[len(plain_text) - 1:]
         return plain_text[:-ord(last_character)]
 
+# RSA implementation
+# Credits: https://pythonhosted.org/pycrypto/Crypto.Cipher.PKCS1_OAEP-module.html
+class RSACipher(object):
+    # Always use the public_key from destiny
+    def encrypt(plain_text, key_path):
+        key = RSA.importKey(open(key_path).read())
+        cipher = PKCS1_OAEP.new(key)
+        
+        # Return is binary
+        return cipher.encrypt(plain_text)
+    
+    # Always use the our secret_key
+    def decrypt(cipher_text, key_path):
+        key = RSA.importKey(open(key_path).read())
+        cipher = PKCS1_OAEP.new(key)
+        
+        # Return is binary
+        return cipher.decrypt(cipher_text)
+
 # Digital Signature
 class DigitalSignature(object):
     def sign(message, secret_key_path):
@@ -70,7 +86,6 @@ class MAC(object):
 
         return h.hexdigest()
 
-
     def validate(secret, message, mac):
         h = HMAC.new(secret, digestmod=SHA256)
         h.update(message)
@@ -80,13 +95,116 @@ class MAC(object):
         else:
             return -1
 
+# Generate RSA Keys
+class RSAGenerator(object):
+    def __init__(self, keys_path):
+        self.keys_path = keys_path
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect((host_IP, host_PORT))
-client_path = os.getcwd()
-keys_path = os.path.join(client_path, 'Keys')
+    def generator(self):
+        random = Random.new().read
+        RSA_key = RSA.generate(2048, random)
+        public_key = RSA_key.publickey().exportKey()
+        secret_key = RSA_key.exportKey()
 
-# Client will receive symetric key
+        public_key_path = os.path.join(keys_path, 'public.pem')
+        secret_key_path = os.path.join(keys_path, 'secret.pem')
+        
+        # Write sk and pk to separated files
+        file = open(public_key_path, "wb")
+        file.write(public_key)
+        file.close()
+        file = open(secret_key_path, "wb")
+        file.write(secret_key)
+        file.close()
+
+if __name__ == "__main__":
+    host_IP = "192.168.1.84"
+    host_PORT = 8080
+    client_path = os.getcwd()
+    keys_path = os.path.join(client_path, 'Keys')
+    
+    
+    # Clients needs sever_pk to work
+    # Checks if there is any pair of RSA keys
+    if os.path.isdir(keys_path):
+        public_key_path = os.path.join(keys_path, 'public.pem')
+        secret_key_path = os.path.join(keys_path, 'secret.pem')
+        server_pk_path = os.path.join(keys_path, 'public_server.pem')
+        if not os.path.isfile(server_pk_path):
+            print('Client nees to have server_pk.pem from trusty source!')
+            sys.exit()    
+        # Needs to check if keys are there
+        if os.path.isfile(public_key_path) and os.path.isfile(secret_key_path):
+            pass
+        else:
+            # Generate RSA Keys
+            rsa = RSAGenerator(keys_path)
+            rsa.generator()
+            public_key_path = os.path.join(keys_path, 'public.pem')
+            secret_key_path = os.path.join(keys_path, 'secret.pem')
+    # If not, creates new pair of RSA Keys and warns the user
+    else:
+        os.mkdir(keys_path)
+        # Generate RSA Keys
+        rsa = RSAGenerator(keys_path)
+        rsa.generator()
+        public_key_path = os.path.join(keys_path, 'public.pem')
+        secret_key_path = os.path.join(keys_path, 'secret.pem')
+
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect((host_IP, host_PORT))
+
+    # 1st Client will send his pk & digital signature
+    #   cipher_text = RSAe(c_pk, s_pk) & ass = Sign(SHA256(c), c_sk)
+    #   c::ass
+    
+    client_pk = RSA.importKey(open(public_key_path).read())
+    client_pk = client_pk.exportKey('PEM')
+    client_sk = RSA.importKey(open(secret_key_path).read())
+    
+    server_pk = RSA.importKey(open(server_pk_path).read())
+    server_pk = server_pk.exportKey('PEM')
+    
+    ass = DigitalSignature.sign(client_pk, secret_key_path)
+    print('aqui')
+    server.send(client_pk)
+    print('ali')
+    server.send(ass)
+    print('acolá')
+    # 2nd Client will receive the symetric key, secret, plus the digital signature from Server
+    #   secret::ass
+    #   secret = RSAd(c, c_sk) & veracity = verify(SHA256(c), s_pk)
+    # secret ==> 256 chr ==> 256*4 = 1024 bits
+    c = server.recv(1024)
+    digital_signature = server.recv(2048)
+
+    # 3rd veracity == True:
+    #   Client starts to use the secret
+    #   Communication is now done with AES
+    secret = RSACipher.decrypt(c, secret_key_path)
+    if DigitalSignature.verify(secret, digital_signature, server_pk_path) == -1:
+        print('Veraficade não verificada!')
+        sys.exit()
+    print('AES agora')
+
+    # 4th Client sends RECEIVE_FLAG to Server with:
+    #   c = AESe(RECEIVE_FLAG, secret)
+    #   hmac = HMAC(SHA256(c), secret)
+    #   c::hmac
+    message_flag = hex(1)
+    print(message_flag)
+    cipher = AESCipher(secret)
+    cipher_text = cipher.encrypt(message_flag)
+    
+    # HMAC
+    hmac = MAC.generate(secret, message_flag.encode())
+    send = cipher_text + "::" + hmac
+    print(send)
+    server.send(send.encode())
+
+'''
+# 5th AES communications until the end of connection
 ciphertext = server.recv(2048)
 
 # Imports Server PK (trusty apriori)
@@ -107,7 +225,9 @@ if DigitalSignature.verify(plaintext.encode(), digital_signature, server_pk_path
     # Client sends OK message
     # Flag == 1, then Client can change messages with Server
     secret = plaintext
+    print(secret)
     message_flag = hex(1)
+    print(message_flag)
     cipher = AESCipher(secret.encode())
     ciphertext = cipher.encrypt(message_flag)
    
@@ -121,7 +241,20 @@ else:
     # Disconnect from Server
     print('FDD')
 
+# Backdoor
+message = 'Olá bro, isto aqui é super seguro....'
 
+# Client OG public key
+client_path = os.getcwd()
+keys_path = os.path.join(client_path, 'Keys')
+rsa = RSAGenerator(client_path)
+rsa.generator()
+
+
+public_key = RSA.importKey(open(public_key_path).read())
+public_key = public_key.exportKey('PEM')
+cipher = AESCipher(public_key)
+#ciphertext = 
 while True:
     1
-    
+'''
--- Server/server.py
+++ Server/server.py
@@ -1,15 +1,17 @@
-
 import signal
 # o que utilizamos
-import binascii, socket, threading, os
+import binascii, socket, threading, os, sys
 from base64 import b64decode, b64encode
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 from Crypto import Random
 from Crypto.Hash import SHA256, HMAC
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, PKCS1_OAEP
 
-# RSA https://www.dlitz.net/software/pycrypto/api/current/Crypto.public_key.RSA-module.html
+# https://docs.python.org/3/library/binascii.html
+from binascii import hexlify
+
+
 # HASH https://pycryptodome.readthedocs.io/en/latest/src/hash/hash.html
 # AES implementation
 # Credits: https://medium.com/quick-code/aes-implementation-in-python-a82f582f51c2
@@ -19,24 +21,26 @@ class AESCipher(object):
         # 16, 24 or 32 bytes
         self.key = SHA256.new(data=key)
         self.key = self.key.digest()
-    
+
     def encrypt(self, plain_text):
         plain_text = self.__pad(plain_text)
         plain_text = plain_text.encode()
         iv = Random.new().read(self.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        encrypted_text = cipher.encrypt(plain_text)
-        return b64encode(iv + encrypted_text).decode("utf-8")
+        cipher_text = cipher.encrypt(plain_text)
+        return b64encode(iv + cipher_text).decode("utf-8")
 
-    def decrypt(self, encrypted_text):
-        encrypted_text = b64decode(encrypted_text)
-        iv = encrypted_text[:self.block_size]
+    def decrypt(self, cipher_text):
+        cipher_text = b64decode(cipher_text)
+        iv = cipher_text[:self.block_size]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        plain_text = cipher.decrypt(encrypted_text[self.block_size:]).decode("utf-8")
+        plain_text = cipher.decrypt(
+            cipher_text[self.block_size:]).decode("utf-8")
         return self.__unpad(plain_text)
 
     def __pad(self, plain_text):
-        number_of_bytes_to_pad = self.block_size - len(plain_text) % self.block_size
+        number_of_bytes_to_pad = self.block_size - \
+            len(plain_text) % self.block_size
         ascii_string = chr(number_of_bytes_to_pad)
         padding_str = number_of_bytes_to_pad * ascii_string
         padded_plain_text = plain_text + padding_str
@@ -47,30 +51,51 @@ class AESCipher(object):
         last_character = plain_text[len(plain_text) - 1:]
         return plain_text[:-ord(last_character)]
 
+# RSA implementation
+# Credits: https://pythonhosted.org/pycrypto/Crypto.Cipher.PKCS1_OAEP-module.html
+class RSACipher(object):
+    # Always use the public_key from destiny
+    def encrypt(plain_text, key_path):
+        key = RSA.importKey(open(key_path).read())
+        cipher = PKCS1_OAEP.new(key)
+        
+        # Return is binary
+        return cipher.encrypt(plain_text)
+    
+    # Always use the our secret_key
+    def decrypt(cipher_text, key_path):
+        key = RSA.importKey(open(key_path).read())
+        cipher = PKCS1_OAEP.new(key)
+        
+        # Return is binary
+        return cipher.decrypt(cipher_text)
+
 # Digital Signature
 class DigitalSignature(object):
     def sign(message, secret_key_path):
         key = RSA.importKey(open(secret_key_path).read())
         h = SHA256.new(data=message)
-        
+
         return PKCS1_v1_5.new(key).sign(h)
 
     def verify(message, message_signed, public_key_path):
         key = RSA.importKey(open(public_key_path).read())
         h = SHA256.new(data=message)
-        
 
         try:
-           PKCS1_v1_5.new(key).verify(h, message_signed)
-           return 1 
+            PKCS1_v1_5.new(key).verify(h, message_signed)
+            return 1
         except(ValueError, TypeError):
             return -1
 
-# HMAC https://pycryptodome.readthedocs.io/en/latest/src/hash/hmac.html
+# HMAC
+# Credits: https://pycryptodome.readthedocs.io/en/latest/src/hash/hmac.html
 class MAC(object):
     def generate(secret, message):
         h = HMAC.new(secret, digestmod=SHA256)
-        return h.update(message)
+        h.update(message)
+
+        return h.hexdigest()
 
     def validate(secret, message, mac):
         h = HMAC.new(secret, digestmod=SHA256)
@@ -81,123 +106,182 @@ class MAC(object):
         else:
             return -1
 
-'''
-# Login
-def ReceiveLoginData(socketClient, AESKey, flag):
-    data = socketClient.recv(1024)
-    # 1st removes the Padding
-    data = RemovePadding(AESKey.decrypt(data))
+# Generate RSA Keys
+# Credits: https://www.dlitz.net/software/pycrypto/api/current/Crypto.PublicKey.RSA-module.html
+class RSAGenerator(object):
+    def __init__(self, keys_path):
+        self.keys_path = keys_path
 
-    # If flag is for creating a new user espectying
-    if data == FLAG_QUIT:
-        username = 'xxx'
-        password = 'xxx'
-        # Checks if the hash is equal
-    else:
-        pass
-
-# Registry
-def ReceiveRegistryData(socketClient, AESKey, flag):
-    data = socketClient.recv(1024)
-    # 1st removes the Padding
-    data = RemovePadding(AESKey.decrypt(data))
-
-    if data == FLAG_QUIT:
-        # Makes a new entry on passwords files!
-        print('ola')
-    else:
-        pass
-'''
-# Handshake
-def Handshake(socketClient):
-    print('asdad')
-
-# Server
-def SettingUp(public_key_path, secret_key_path):
-    while True:
-        # Host and Client data for debugging
-        host, client = server.accept()
-     
-        # Generate a simetric secret in hex (s)
-        # Each char ===> 4 bytes
-        # So 128*4 = 512 bytes
-        # State of Art
-        # AES with 128 bits == 3072 bits in RSA == 256 bits in ECC
-        # 32 chars ===> 128 bits
-        secret = binascii.b2a_hex(os.urandom(32)).hex()
-        print('Segredo')
-        print(secret)
-        
-        # Cipher with AES c = (AES(s, pk_s))
-        public_key = RSA.importKey(open(public_key_path).read())
-        public_key = public_key.exportKey('PEM')
-        
-        cipher = AESCipher(public_key)
-        ciphertext = cipher.encrypt(secret)
-        
-        # Every data need to be binary to send to client
-        host.send(ciphertext.encode())
-        
-        
-        # Digital Signature h, sign = Sign(h(c), sk_s)
-        digital_signature = DigitalSignature.sign(ciphertext.encode(), secret_key_path)
-        host.send(digital_signature)
-        # Sends to client, send(c, sign)
-        ciphertext = host.recv(2048)
-
-        cipher = AESCipher(secret.encode())
-        plaintext = cipher.decrypt(ciphertext)
-        print(plaintext)
-        hmac = host.recv(2048)
-
-        # Calculate the MAC validation
-        i = MAC.validate(secret.encode(), plaintext.encode(), hmac)
-        print(i)
-        
-
-if __name__ == "__main__":
-    # IP and Port of Server
-    host_IP = "192.168.207.15"
-    host_PORT = 8080
-    server_path = os.getcwd()
-    keys_path = os.path.join(server_path, 'Keys')
-
-    # Checks if there is any pair of RSA keys
-    if os.path.isdir(keys_path):
-        # Needs to check if keys are there
-        public_key_path = os.path.join(keys_path, 'public.pem')
-        secret_key_path = os.path.join(keys_path, 'secret.pem')
-    # If not, creates new pair of RSA Keys and warns the user
-    else:
-        os.mkdir(keys_path)
+    def generator(self):
         random = Random.new().read
         RSA_key = RSA.generate(2048, random)
-        public_key = RSA_key.public_key().exportKey()
+        public_key = RSA_key.publickey().exportKey()
         secret_key = RSA_key.exportKey()
 
         public_key_path = os.path.join(keys_path, 'public.pem')
         secret_key_path = os.path.join(keys_path, 'secret.pem')
-        # Writes
-
+        
+        # Write sk and pk to separated files
         file = open(public_key_path, "wb")
         file.write(public_key)
         file.close()
         file = open(secret_key_path, "wb")
         file.write(secret_key)
         file.close()
+
+# Global Variables
+secret = ''
+# Threadpool ===> [(c1_IP, c1_secret), (c2_IP, c2_secret),..., (cn_IP, cn_secret)]
+threadpool = []
+
+# Handshake with Digital Signature
+def Handshake(host, public_key_path, secret_key_path):
+    # 1st Server will receive from the Client the c_pk & digital signature:
+    #      c_pk & ass = Sign(SHA256(c_pk), c_sk)
+    #      c
+    #      ass
+    c_pk = host.recv(2048)
+    digital_signature = host.recv(2048)
+    client_pk_path = os.path.join(keys_path, 'public_client.pem')
+    file = open(client_pk_path, "wb")
+    file.write(c_pk)
+    file.close()
+
+    # 2nd Server will check the veracity of recived digital signature: 
+    #      veracity = DigitalSignature.verify(c_pk, ass, c_pk)
+    if DigitalSignature.verify(c_pk, digital_signature, client_pk_path) == -1:    
+        print('Verificade não verificada!')
+        os.remove(client_pk_path)
+        # --------------------------------------- #
+        # CLOSES THE THREAD
+        # Closes that thread
+        sys.exit()
+
+    
+    # 3rd Server will generate a simetric secret, secret, in hex
+    #      Each char ===> 4 bytes
+    #      So 128*4 = 512 bytes
+    #      State of Art
+    # AES with 128 bits == 3072 bits in RSA == 256 bits in ECC
+    # 32 chars ===> 128 bits
+    secret = binascii.b2a_hex(os.urandom(32)).hex()
+
+    # 4th Server will cipher the secret, c, with RSA and sign it: 
+    #      c = RSAe(secret, c_pk) & ass = Sign(SHA256(c), s_sk)
+    cipher_text = RSACipher.encrypt(secret.encode(), client_pk_path)
+    ass = DigitalSignature.sign(cipher_text, secret_key_path)
+
+    host.send(cipher_text)
+    host.send(ass)
+
     '''
-    server = ""
-    AESKey = ""
-    RecDados = []
-    CONNECTION_LIST = []
-    FLAG_READY = "Ready"
-    FLAG_QUIT = "quit"
+    # 5th Wait for confirmation from Client with AES
+ 
+    
+    cipher = AESCipher(secret_key_path)
+    cipher_text = cipher.encrypt(secret)
+
+    # Every data need to be binary to send to client
+    host.send(cipher_text.encode())
+
+    # Digital Signature h: sign = Sign(h(c), sk_s)
+    digital_signature = DigitalSignature.sign(
+        cipher_text.encode(), secret_key_path)
+    host.send(digital_signature)
+    
+    # Wait for affirmative response from Client 
+    ReceiveMessages(host, secret)
+    print('Mensagem recebida!')
+    # If message is affirmative
+    # Saves on threadpool -> client_IP + secret
+    threadpool.append(host, secret)
+    print(threadpool)
+    print('Feito handshake')
     '''
+
+# Receive Messages (Client -> Server) 
+def ReceiveMessages(host, secret):
+    cipher_text = host.recv(2048)
+    # Decode with AES: m = AESd(c, s)
+    cipher = AESCipher(secret.encode())
+    plain_text = cipher.decrypt(cipher_text)
+    print(plain_text)
+    hmac = host.recv(2048)
+
+    # Calculate the MAC validation
+    i = MAC.validate(secret.encode(), plain_text.encode(), hmac)
+    print(i)
+
+# Send Messages (Sever -> All Clients)
+# Needs threadpool
+def SendMessages(host, secret, message):
+    cipher = AESCipher(secret.encode())
+    cipher_text = cipher.encrypt(message)
+
+    host.send(cipher_text.encode())
+    
+    hmac = MAC.generate(secret.encode(), message.encode())
+    host.send(hmac.encode())
+
+# Server
+def SettingUp(server_public_key_path, server_secret_key_path):
+    while True:
+        # Host and Client data for debugging
+        host, client = server.accept()
+        print(host)
+        print(client)
+        
+        print('Handshake Phase')
+        # Handshake
+        Handshake(host, server_public_key_path, server_secret_key_path)
+        
+        print('feito')
+        global secret
+        # Change of messages
+        message = 'Bora meter lume nisto'
+        print(message)
+        message = message.encode('utf-8').hex()
+        print(message)
+        SendMessages(host, secret, message)
+ 
+
+if __name__ == "__main__":
+    # IP and Port of Server
+    host_IP = "192.168.1.84"
+    host_PORT = 8080
+    server_path = os.getcwd()
+    keys_path = os.path.join(server_path, 'Keys')
+    print('sup')
+    # Checks if there is any pair of RSA keys
+    if os.path.isdir(keys_path):
+        public_key_path = os.path.join(keys_path, 'public.pem')
+        secret_key_path = os.path.join(keys_path, 'secret.pem')
+        
+        # Needs to check if keys are there
+        if os.path.isfile(public_key_path) and os.path.isfile(secret_key_path):
+            pass
+        else:
+            # Generate RSA Keys
+            rsa = RSAGenerator(keys_path)
+            rsa.generator()
+            public_key_path = os.path.join(keys_path, 'public.pem')
+            secret_key_path = os.path.join(keys_path, 'secret.pem')
+    # If not, creates new pair of RSA Keys and warns the user
+    else:
+        os.mkdir(keys_path)
+        # Generate RSA Keys
+        rsa = RSAGenerator(keys_path)
+        rsa.generator()
+        public_key_path = os.path.join(keys_path, 'public.pem')
+        secret_key_path = os.path.join(keys_path, 'secret.pem')
+    
     # Starting Up the Server
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind((host_IP, host_PORT))
     server.listen(0)
+    # sys.exit()
+    
     # accept clients
     threading_accept = threading.Thread(
         target=SettingUp, args=[public_key_path, secret_key_path])
